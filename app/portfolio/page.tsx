4b7ad90c48665cbfc2fa9bb40a89e99deee33a0b
mikethompson100 <mikethompson100@hotmail.com>
2025-11-21 09:47:48 -0500

    Initial setup

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mike Thompson | Portfolio",
+  description: "Portfolio",
+};
+export default function Portfolio() {
+
+return (<h2>Portfolio</h2>)
+
+}
